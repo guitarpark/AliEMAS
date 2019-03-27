9176be00af147b250349411ad06fa0e503199610
guita <guita@DESKTOP-E0KPDF9>
2019-03-27 18:38:56 +0800

    初始化iOS项目

--- AliEMASBinding/AliEMAS.Binding.iOS/AlicloudMobileAnalitics.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith("libEmptyLibrary.a", LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreTelephony SystemConfiguration", LinkerFlags = "-ObjC -lz -lresolv -lsqlite3")]
