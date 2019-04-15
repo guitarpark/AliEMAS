64a2c556f23f456d003e7070aaf8f17a67dcb95f
刘辉 <guitarpark@outlook.com>
2019-04-15 14:22:04 +0800

    修正版本号

--- AliEMASBinding/AliEMAS.Binding.Droid/Properties/AssemblyInfo.cs
+++ AliEMASBinding/AliEMAS.Binding.Droid/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 
 #region 阿里云推送相关
